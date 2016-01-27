b737ae4a258934fc7d1f95c77313b61f190ef65b
Fumitoshi Ogata <ogata.fumitoshi@o-05917-mac.local>
2016-01-27 16:53:11 +0900

    first commit

---
+++ Cockpit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cockpit
+//
+//  Created by Fumitoshi Ogata on 2016/01/27.
+//  Copyright © 2016年 Fumitoshi Ogata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
